2518423d69053109e764abb6729c237c6f4ab337
Guillermo <grlara9@gmail.com>
2021-11-11 22:01:11 -0600

    apply style to table and the overall app

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './Table.css'
 
 function Table({countries}) {
     return (
         <div className="table">
+            <h2>Cases By Country</h2>
             {countries.map(({country, cases}) =>(
                 <tr>
                     <td>{country}</td>
