2f05da67d00a9a6eb6114528c8ee173f82695e3e
Guillermo <grlara9@gmail.com>
2021-11-11 23:46:47 -0600

    import react bootstrap to style

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,6 +1,6 @@
 export const sortData = (data) =>{
     const sortedData = [...data]
-return sortedData.sort((a,b) => (a.cases > b.cases ? -1 : 1))
+    return sortedData.sort((a,b) => (a.cases > b.cases ? -1 : 1))
    /* sortedData.sort((a,b) =>{
         if(a.cases > b.cases){
             return -1
